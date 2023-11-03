51183592d04d3baa57dd41fae167db981c5b1b4a
Nguyen Gia Hoang <popemkt1@gmail.com>
2023-11-03 11:21:15 +0700

    Finish deadlock

--- DeadLock/Program.cs
+++ DeadLock/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Parallel.Invoke(Thread1, Thread2);
 
-Console.WriteLine("Hello, World!");
+void Thread1()
+{
+    lock (typeof(int))
+    {
+        Console.WriteLine("thread 1 got int lock");
+        Thread.Sleep(1000);
+        lock (typeof(long))
+            Console.WriteLine("thread 1 got both locks");
+        // do something
+    }
+}
+
+void Thread2()
+{
+    lock (typeof(long))
+    {
+        Console.WriteLine("thread 2 got long lock");
+        Thread.Sleep(1000);
+        lock (typeof(int))
+            Console.WriteLine("thread 2 got both locks"); // do something
+    }
+}
